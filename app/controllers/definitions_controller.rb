92693576fb9742a2b04d195e16f8e8e00fa74d55
showwin <showwin_kmc@yahoo.co.jp>
2015-02-12 22:36:38 +0900

    複数定義書番号対応

--- app/controllers/definitions_controller.rb
+++ app/controllers/definitions_controller.rb
@@ -3,7 +3,7 @@ class DefinitionsController < ApplicationController
   before_action :set_form_params, only: [:new, :edit]
 
   def show
-    @logs = ChangeLog.where(指標番号: params[:id]).to_a
+    @logs = ChangeLog.where(_id: params[:id]).to_a
   end
 
   def new
@@ -24,7 +24,7 @@ class DefinitionsController < ApplicationController
     confirm
 
     # 検索用のレコード作成 と 定義の作成
-    if @messages.blank? && (@definition.create_search_index(params) && @definition.save) 
+    if @messages.blank? && (@definition.create_search_index(params) && @definition.save)
       render :success
     else
       set_form_params
@@ -33,7 +33,7 @@ class DefinitionsController < ApplicationController
   end
 
   def edit
-    @edit_logs = ChangeLog.where(指標番号: params[:id]).to_a
+    @edit_logs = ChangeLog.where(_id: params[:id]).to_a
   end
 
   def update
@@ -65,18 +65,20 @@ class DefinitionsController < ApplicationController
 
   private
     def set_definition
-      @definition = Definition.where(指標番号: params[:id]).first
+      @definition = Definition.where(_id: params[:id]).first
     end
 
     def set_form_params
+      @projects = [['QIP', 'qip'], ['日病', 'jha'], ['全自病協', 'jmha'], ['済生会', 'sai'], ['全日本民医連', 'min'],
+                   ['日本医師会', 'jma'], ['全日病', 'ajha'], ['国病', 'nho'], ['労災', 'rofuku'], ['慢医協', 'jamcf']]
       @years = ['2008', '2010', '2012', '2014']
       @dataset = ['DPC様式1', 'Fファイル', 'EFファイル', 'Dファイル']
     end
 
     def confirm
-      if @definition['指標番号'].blank?
-        @messages['指標番号'] = '指標番号を記入して下さい'
-      end
+      #if @definition['指標番号'].blank?
+      #  @messages['指標番号'] = '指標番号を記入して下さい'
+      #end
       if @log['変更者'].blank?
         @messages['変更者'] = '変更者を記入して下さい'
       end

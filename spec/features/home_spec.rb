62aa22435d6dbdbf46b47398d9155f5754164adf
hayayu11 <galatasaray.vs.lazio@gmail.com>
2017-07-14 18:17:04 +0900

    modify spec files and pdf design slightly

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -15,6 +15,7 @@ RSpec.describe HomeController, type: :feature do
     click_button('　検　索　')
 
     # 検索結果
+    expect(page).to have_content('検索結果: 1 件') 
     expect(page).to have_content('指標番号: 64 (qip)')
     expect(page).to have_content('2 (jha)')
     expect(page).to have_content('指標群: 呼吸器系')
